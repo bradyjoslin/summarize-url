f42fa25870d44688cfb6ff5488fa46e1595837a6
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-06-02 03:00:15 +0000

    update algorithmia dependency

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,13 @@ fn main() -> AppResult<()> {
 }
 
 fn summarize_url(api_key: &str, url: &str, length: &i32) -> AppResult<String> {
-    let client = Algorithmia::client(api_key);
-    let algo = client.algo("nlp/SummarizeURL/0.1.4");
+    let client = match Algorithmia::client(api_key) {
+        Ok(c) => Ok(c),
+        Err(_) => Err(Error::AlgoAPI),
+    };
+
+    let algo = client?.algo("nlp/SummarizeURL/0.1.4");
+
     let _l = length.to_owned().to_string();
     let input_cfg = &vec![url, &_l];
     match algo.pipe(input_cfg) {
